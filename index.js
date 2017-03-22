49086f9ef164c36ab3b3cb69adfc950decc1f10a
hwalborn <hwalborn@gmail.com>
2017-03-22 00:17:48 -0400

    API

--- index.js
+++ index.js
@@ -6,3 +6,33 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories(){
+  search = "https://api.github.com/search/repositories?q=" + document.getElementById("searchTerms").value
+  $.get(search, (response) =>{
+    const src = document.getElementById("repository-template").innerHTML
+    const template = Handlebars.compile(src)
+    const repoList = template(response.items)
+    $("#results").html(repoList)
+  }).fail(displayError())
+}
+
+function displayError(){
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+
+function showCommits(el) {
+  url = `https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`
+  $.get(url, (response) => {
+    const src = document.getElementById("commit-template").innerHTML
+    const template = Handlebars.compile(src)
+    const commitList = template(response)
+    $("#details").html(commitList)
+  })
+}
+
+/////Handlebar Partial/////
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial("userDetails", document.getElementById("user-details-partial").innerHTML)
+});
